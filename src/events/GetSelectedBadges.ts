d9106fdc3a686015f2c76899ad4f41fd7d750b25
caiotito <caiocalheiros.o@gmail.com>
2025-01-20 08:54:35 -0300

    Notificacoes traduzidas

--- src/events/GetSelectedBadges.ts
+++ src/events/GetSelectedBadges.ts
@@ -19,7 +19,7 @@ export const run = (ext: Extension , hMessage: HMessage) => {
                 }
             }, 100)
             setClickFloodInterval(int)
-            sendNotification(`Enabled floodclick ${user ? `on ${user.name}` : "but i noticed that roomUsers is empty, rejoin the room to fix it"}`)
+            sendNotification(`Habilitado floodclick ${user ? `em ${user.name}` : "mas notei que roomUsers está vazio, reentre na sala para consertar"}`)
         } else if (ext.state.handItemFlood) {
             let int = setInterval(() => {
                 if (ext.state.selectedUser && ext.state.handItemFlood) {
@@ -28,7 +28,7 @@ export const run = (ext: Extension , hMessage: HMessage) => {
                 }
             }, 75)
             setHanditemFloodInterval(int)
-            sendNotification(`Enabled handitem flood ${user ? `on ${user.name}` : "but i noticed that roomUsers is empty, rejoin the room to fix it"}`)
+            sendNotification(`Habilitado flood de handitem ${user ? `em ${user.name}` : "mas notei que roomUsers está vazio, reentre na sala para consertar"}`)
         }
     }
 
@@ -37,7 +37,7 @@ export const run = (ext: Extension , hMessage: HMessage) => {
     const user = ext.roomUsers.find(user => user.id === id)
 
     if (!user) {
-        return sendNotification(`i blocked your click but cant find the user on my users list, rejoin the room to fix it`)
+        return sendNotification(`bloqueei seu clique, mas não consigo encontrar o usuário na minha lista de usuários, reentre na sala para consertar`)
     } else if (user.id === ext.userObject.id) return;
 
     hMessage.blocked = true;
